5194c5c1a9f5a5b470a7fe58ee40e04e27d2dc2e
gertmask <dtepam@gmail.com>
2018-08-17 12:30:29 +0300

    project structure (mvn + spring boot)

---
+++ src/main/java/com/gertmask/umldap/UmLdapApplication.java
@@ -0,0 +1,12 @@
+package com.gertmask.umldap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmLdapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UmLdapApplication.class, args);
+	}
+}
---
+++ src/test/java/com/gertmask/umldap/UmLdapApplicationTests.java
@@ -0,0 +1,16 @@
+package com.gertmask.umldap;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UmLdapApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
